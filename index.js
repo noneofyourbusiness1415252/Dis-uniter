9ed9bd70e2976165de6797d86d7643fa42960e8c
Umar Sharief <80610051+noneofyourbusiness1415252@users.noreply.github.com>
2022-08-23 17:23:54 +0000

    oops

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits } = require("discord.js"),
   bot = new Client({ intents: [GatewayIntentBits.Guilds] });
-const fs = require("fs");
 require("./dis-uniter")(bot);
 bot.login();
