946a6c0a4a1889888208cafe5e6116081299931d
Umar Sharief <80610051+noneofyourbusiness1415252@users.noreply.github.com>
2022-09-20 21:19:34 +0000

    markdown description, log warnings and errors

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-This does literally everything needed to keep your bot alive on replit. It creates a website that displays info about your bot. Example usage:
+![npm bundle size](https://img.shields.io/bundlephobia/minzip/replit-dis-uniter) ![GitHub package.json version](https://img.shields.io/github/package-json/v/noneofyourbusiness1415252/dis-uniter)
+This does literally everything needed to keep your bot alive on replit. It creates a website that displays info about your bot. Quick setup: enter this in shell, then run your bot:
 
-```js
-const { Client, GatewayIntentBits } = require("discord.js"),
-  client = new Client({ intents: [GatewayIntentBits.Guilds] });
-require("replit-dis-uniter")(client);
-client.login();
+```bash
+sed -i '/.*client.login(.*/s/^/require("replit-dis-uniter")(client);\n/' *.js
 ```
 
 [Here](https://626110f0-d408-475b-9830-1d15b93582e1.id.repl.co) is a website of a bot kept alive by this package
--- index.js
+++ index.js
@@ -2,34 +2,33 @@ const { createServer } = require("http"),
   { get } = require("https"),
   { execSync } = require("child_process"),
   { env } = process,
-  { loadavg } = require("os");
+  { loadavg } = require("os"),
+  { appendFileSync } = require("fs");
+function handleRateLimit({ timeToReset, timeout, global }) {
+  if ((timeToReset ?? timeout) > 10000 && !global) {
+    process.emitWarning("Rate limit: restarting");
+    get(
+      `https://cd594a2f-0e9f-48f1-b3eb-e7f6e8665adf.id.repl.co/${env.REPL_ID}`,
+      () => process.kill(1)
+    );
+  }
+}
+const log = (msg) => appendFileSync("log", `${Date.now()}: ${msg}\n`);
 module.exports = (/**@type{Client}*/ bot) => {
-  bot.rest.on?.("rateLimited", ({ timeToReset, global }) => {
-    if (timeToReset > 10000 && !global) {
-      process.emitWarning("Rate limit: restarting");
-      get(
-        `http://cd594a2f-0e9f-48f1-b3eb-e7f6e8665adf.id.repl.co/${env.REPL_ID}`,
-        () => process.kill(1)
-      );
-    }
-  }) ??
-    bot.on("rateLimit", ({ timeout, global }) => {
-      if (timeout > 10000 && !global) {
-        process.emitWarning("Rate limit: restarting");
-        get(
-          `https://cd594a2f-0e9f-48f1-b3eb-e7f6e8665adf.id.repl.co/${env.REPL_ID}`,
-          () => process.kill(1)
-        );
-      }
-    });
+  bot.on("warn", log);
+  bot.on("error", log);
+  if (bot.readyAt)
+    process.emitWarning("Oops. You put this after `client.login()`");
+  bot.rest.on?.("rateLimited", handleRateLimit) ??
+    bot.on("rateLimit", handleRateLimit);
   bot.once("ready", async () => {
     await bot.application.fetch();
     await bot.user.fetch(false);
     const { application, user, presence } = bot;
-    /**@type{User}*/ let owner =
-      application.owner.owner?.user ?? application.owner;
+    let owner = application.owner.owner?.user ?? application.owner;
     await owner.fetch(false);
     createServer((_, res) => {
+      const description = application.description.replace(/"/g, "\\");
       owner = application.owner.owner?.user ?? application.owner;
       res.writeHead(200, {
         "Content-Type": "text/html;charset=utf-8",
@@ -41,17 +40,15 @@ module.exports = (/**@type{Client}*/ bot) => {
           application.description
         }'><meta name=author content='${owner}'><meta name=twitter:image content=${user.avatarURL()}><title>${
           user.tag
-        }</title><link rel='shortcut icon' href='${user.displayAvatarURL()}''><script>onload=()=>{document.getElementById('s').textContent=new Date(${
+        }</title><link rel=icon href=${user.displayAvatarURL()}><script src=https://cdn.jsdelivr.net/gh/adamvleggett/drawdown/drawdown.min.js></script><script>onload=()=>{document.getElementById('s').textContent=new Date(${
           bot.readyTimestamp
         }).toLocaleString();setInterval(()=>{let u=BigInt(Math.floor((Date.now()-${
           bot.readyTimestamp
-        })/1000));document.getElementById('u').innerText=\`\${u>86400n?\`\${u/86400n}d\`:''}\${u>3600n?\`\${u/3600n%60n}h\`:''}\${u>60n?\`\${u/60n%24n}m\`:''}\${\`\${u%60n}\`}s\`}, 1000)}</script><style>*{background-color:#FDF6E3;color:#657B83;font-family:sans-serif;text-align:center;margin:auto}@media(prefers-color-scheme:dark){*{background-color:#002B36;color:#839496}}img{height:1em}td{border:1px}</style><html lang=en><h1>${
+        })/1000));document.getElementById('u').innerText=\`\${u>86400n?\`\${u/86400n}d\`:''}\${u>3600n?\`\${u/3600n%60n}h\`:''}\${u>60n?\`\${u/60n%24n}m\`:''}\${\`\${u%60n}\`}s\`}, 1000);document.getElementById('d').innerHTML=markdown("${description}")}</script><style>*{background-color:#FDF6E3;color:#657B83;font-family:sans-serif;text-align:center;margin:auto}@media(prefers-color-scheme:dark){*{background-color:#002B36;color:#839496}}img{height:1em}td{border:1px}</style><html lang=en><h1>${
           user.tag
-        }<img src='${bot.user.avatarURL()}'alt></h1><p>${
-          application.description
-        }</p><table><tr><td>Channels<td>${
-          bot.channels.cache.size
-        }<tr><td>Guilds<td>${bot.guilds.cache.size}<tr><td>Ping<td>${
+        }<img src='${bot.user.avatarURL()}'alt></h1><p id=d><table><tr><td>Guilds<td>${
+          bot.guilds.cache.size
+        }<tr><td>Ping<td>${
           bot.ws.ping
         }ms<tr><td>Up since<td id=s><tr><td>Uptime<td id=u><tr><td>Status<td>${
           presence.status
@@ -81,15 +78,12 @@ module.exports = (/**@type{Client}*/ bot) => {
       bot.user.fetch();
       bot.application
         .fetch()
-        .then(({ owner }) =>
-          /**@type{User}*/ (owner.owner?.user ?? owner).fetch()
-        );
+        .then(({ owner }) => (owner.owner?.user ?? owner).fetch());
     });
   });
 };
 if (__dirname == process.cwd()) {
-  const { Client, GatewayIntentBits, User } = require("discord.js"),
-    bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+  const bot = new (require("discord.js").Client)({ intents: 0 });
   module.exports(bot);
   bot.login();
 }
