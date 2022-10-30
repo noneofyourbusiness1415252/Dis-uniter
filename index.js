23ffa1b071c0d9ba997c5fc65e71f57c4ae09cc6
Umar Sharief <80610051+noneofyourbusiness1415252@users.noreply.github.com>
2022-10-30 14:59:10 +0000

    number of bug fixes from last terrible release

--- index.js
+++ index.js
@@ -23,15 +23,23 @@ module.exports = (/**@type{Client}*/ bot) => {
       .fetch()
       .then(({ owner }) => (owner.owner?.user ?? owner).fetch());
     application.HTMLDescription = application.description
-      .replace(/(?<!\\)<(?!t:)(.+)(?<!\\)>/g, "<a href='$1'>$1</a>")
-      .replace(/(?<!\*|\\)\*([^\*]+)(?<!\*|\\)\*/g, "<i>$1</i>")
-      .replace(/(?<!\\)\*\*([^\*]+)(?<!\\)\*\*/g, "<b>$1</b>")
-      .replace(/(?<!\\)_([^\_]+)(?<!\\)_/g, "<u>$1</u>")
-      .replace(/(?<!\\)~~([^\~]+)(?<!\\)~~/g, "<strike>$1</strike>")
-      .replace(/(?<!\\)\|\|([^\\|]+)(?<!\\)\|\|/g, "<details>$1</details>")
+      .replace(
+        /(?<!\\)(`(?:``|))(.+?)\1/gs,
+        (_, _delim, code) =>
+          String.raw`\<pre\>${code
+            .replace(/\*|_|~~|\|\||/, "\\$&")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")}\</pre\>`
+      )
+      .replace(/(?<!\\)<(?!t:)(.+?)(?<!\\)>/g, "<a href='$1'>$1</a>")
+      .replace(/(?<!\*|\\|<pre>.*(?!<\/pre>))\*(.+?)(?<!\*|\\)\*/g, "<i>$1</i>")
+      .replace(/(?<!\\)\*\*(.+?)(?<!\\)\*\*/g, "<b>$1</b>")
+      .replace(/(?<!\\)_(.+?)(?<!\\)_/g, "<u>$1</u>")
+      .replace(/(?<!\\)~~(.+?)(?<!\\)~~/g, "<strike>$1</strike>")
+      .replace(/(?<!\\)\|\|(.+?)(?<!\\)\|\|/g, "<details>$1</details>")
       .replace(/^>>>(.+)/ms, "<blockquote><p>$1</blockquote>")
-      .replace(/^> (.+)/gm, "<blockquote><p>$1</blockquote>")
-      .replace(/(?<!\\)`(``|)([^\`]+)`(``|)/gs, "<pre>$2</pre>");
+      .replace(/^> (.+?)/gm, "<blockquote><p>$1</blockquote>")
+      .replace(/\\/g, "");
     const { installParams, customInstallURL } = application;
     application.install = customInstallURL
       ? `<a href=${customInstallURL}>Install</a>`
@@ -100,22 +108,23 @@ module.exports = (/**@type{Client}*/ bot) => {
       owner = application.owner.owner?.user ?? application.owner;
       res.writeHead(200, {
         "Content-Type": "text/html;charset=utf-8",
-        "Cache-Control": "no-cache",
+        "Cache-Control": req.url == "/" ? "no-cache" : "max-age=180",
         "X-Content-Type-Options": "nosniff",
       });
       res.end(
         {
-          err: `${`${readFileSync("log")}`
+          err: `${`${readFileSync("/proc/meminfo")}`.match(
+            /(?<=le:.*)\d+/
+          )}B${`${readFileSync("log")}`
             .split(
               `
 `
             )
             .filter((line) => line[0] == "⚠").join(`
 `)}`,
-          dbg: `RAM available: ${`${readFileSync("/proc/meminfo")}`.match(
+          dbg: `${`${readFileSync("/proc/meminfo")}`.match(
             /(?<=le:.*)\d+/
-          )}B
-${readFileSync("log")}`,
+          )}B${readFileSync("log")}`,
           "": `<!DOCTYPE html><meta charset=utf-8><meta name=viewport content='width=device-width'><meta name=description content='${
             application.description
           }'><meta name=author content='${owner}'><meta name=twitter:image content=${user.avatarURL(
@@ -124,14 +133,7 @@ ${readFileSync("log")}`,
             user.tag
           }</title><link rel=icon href=${user.displayAvatarURL({
             extension: "png",
-          })}><script>let n,t;function l(){let x=new XMLHttpRequest();x.open("GET",document.getElementById('s').innerText[0]=='S'?'err':'dbg');x.onload=r=>{document.getElementById('l').innerText=r.srcElement.responseText.replace(/\w*([0-9]+): /g,(_, d)=>\`$\{new Date(d/1).toLocaleString()\}: \`)};x.send()};document.onvisibilitychange=()=>{if(document.visibilityState=="hidden")clearInterval(n);else n=setInterval(l,5e3)};onload=()=>{document.getElementById('r').textContent=new Date(${
-            bot.readyTimestamp
-          }).toLocaleString();setInterval(()=>document.getElementById('u').innerText=new Date(Date.now()-${
-            bot.readyTimestamp + 36e5
-          }).toLocaleTimeString(),1e3);n=setInterval(l,5e3);let d=document.getElementById('d'),f=new Intl.RelativeTimeFormat();d.innerHTML=\`${application.HTMLDescription.replace(
-            /`/g,
-            "\\`"
-          )}\`.replace(/<t:(\\d+):R>/g,(_,t)=>{const r=t-Date.now(),a=Math.abs(r);return \`<abbr title="\${new Date(t/1).toLocaleString(0,{timeStyle:"short",dateStyle:"full"})}">\${a>31536e6?f.format(r/31536e6,"year"):a>7884e6?f.format(r/7884e6,"quarter"):a>2628e6?f.format(r/2628e6,"month"):a>6048e5?f.format(r/6048e5,"week"):a>864e5?f.format(r/864e5,"day"):a>36e5?f.format(r/36e5,"hour"):a>6e4?f.format(r/6e4,"minute"):f.format(r/1e3,"second")}</abbr>\`}).replace(/<t:(\\d+)(:[tdf])?>/gi,(_,t,m)=>{const d=Date.prototype.toLocaleString.bind(new Date(t/1),0);m=m?.[1];return \`<abbr title="\${d({timeStyle:"short",dateStyle:"full"})}">\${d({timeStyle:{t:"short",T:"long"}[m],dateStyle:{d:"short",D:"long"}[m]})}</abbr>\`})}</script><style>blockquote>*{border-right:4px solid #FDF6E3;border-left:4px solid #EEE8D5;display:inline;padding:4px 4px;border-radius:1px;border-color:}*:not(pre){box-sizing:border-box;background-color:#FDF6E3;color:#657B83;font-family:sans-serif;text-align:center;margin:0 auto}@media(prefers-color-scheme:dark){*:not(pre){background-color:#002B36;color:#839496}blockquote>*{border-color:#073642}}img{height:1em}td{border:1px}pre{display:inline}p{white-space:pre-wrap} body{display:flex;flex-wrap:wrap;height:100vh}#o{flex:1;min-width:min-content}#o,#v{overflow:auto}button{display:inline-block}a{color:#268BD2}</style><html lang=en><div id=o><h1>${
+          })}><style>blockquote>*{border-right:4px solid #FDF6E3;border-left:4px solid #EEE8D5;display:inline;padding:4px 4px;border-radius:1px;border-color:}*:not(pre){background-color:#FDF6E3;color:#657B83;font-family:sans-serif;text-align:center;margin:0 auto}@media(prefers-color-scheme:dark){*:not(pre){background-color:#002B36;color:#839496}blockquote>*{border-color:#073642}}img{height:1em}td{border:1px}pre{display:inline}p{white-space:pre-wrap}body{display:flex;flex-flow:row wrap;width:100vw}#o,#v{overflow:auto}#o{min-width:min-content;flex:1}#v{height:100vh}button{display:inline-block}a{color:#268BD2}</style><html lang=en><div id=o><h1>${
             user.tag
           }<img src='${bot.user.avatarURL({
             extension: "png",
@@ -155,7 +157,14 @@ ${readFileSync("log")}`,
             "/proc/meminfo"
           )}`.match(/(?<=le:.*)\d+/)}</table>${
             application.install
-          }<p></div><div id=v><div><button type=button id=s onclick="document.getElementById('s').innerText=\`\${document.getElementById('s').innerText[0]=='S'?'Hide':'Show'} debug\`">Show debug</button><button type=button onclick="let l=document.getElementById('l');if(l.style.height){l.style.height='';document.getElementById('o').style.display=''}else{l.style.height='100vh';document.getElementById('o').style.display='none'}">Toggle info</button></div><pre id=l></div>`,
+          }<p></div><div id=v><button type=button id=s onclick="document.getElementById('s').innerText=\`\${document.getElementById('s').innerText[0]=='S'?'Hide':'Show'} debug\`">Show debug</button><p><pre id=l></div><script>try{let n,t;function l(){let x=new XMLHttpRequest();x.open("GET",document.getElementById('s').innerText[0]=='S'?'err':'dbg');x.onload=r=>{document.getElementById('l').innerText=r.srcElement.responseText.replace(/.+?B/, r=>{document.querySelector("tr:last-of-type td").innerText=r;return""}).replace(/\w*([0-9]+): /g,(_, d)=>\`$\{new Date(d/1).toLocaleString()\}: \`)};x.send()};document.onvisibilitychange=()=>{if(document.visibilityState=="hidden")clearInterval(n);else n=setInterval(l,5e3)};document.getElementById('r').textContent=new Date(${
+            bot.readyTimestamp
+          }).toLocaleString();setInterval(()=>{let u=BigInt(Math.floor((Date.now()-${
+            bot.readyTimestamp
+          })/1000));document.getElementById('u').innerText=\`\${u>86400n?\`\${u/86400n}d\`:''}\${u>3600n?\`\${u/3600n%60n}h\`:''}\${u>60n?\`\${u/60n%24n}m\`:''}\${\`\${u%60n}\`}s\`}, 1000);;n=setInterval(l,5e3);let d=document.getElementById('d'),f=new Intl.RelativeTimeFormat();d.innerHTML=\`${application.HTMLDescription.replace(
+            /`/g,
+            "\\`"
+          )}\`.replace(/<t:(\\d+):R>/g,(_,t)=>{const r=t-Date.now(),a=Math.abs(r);return \`<abbr title="\${new Date(t/1).toLocaleString(0,{timeStyle:"short",dateStyle:"full"})}">\${a>31536e6?f.format(r/31536e6,"year"):a>7884e6?f.format(r/7884e6,"quarter"):a>2628e6?f.format(r/2628e6,"month"):a>6048e5?f.format(r/6048e5,"week"):a>864e5?f.format(r/864e5,"day"):a>36e5?f.format(r/36e5,"hour"):a>6e4?f.format(r/6e4,"minute"):f.format(r/1e3,"second")}</abbr>\`}).replace(/<t:(\\d+)(:[tdf])?>/gi,(_,t,m)=>{const d=Date.prototype.toLocaleString.bind(new Date(t/1),0);m=m?.[1];return \`<abbr title="\${d({timeStyle:"short",dateStyle:"full"})}">\${d({timeStyle:{t:"short",T:"long"}[m],dateStyle:{d:"short",D:"long"}[m]})}</abbr>\`})}catch(err){open(\`data:text/plain,\${err}\`,"_blank")}</script>`,
         }[req.url.slice(1)]
       );
     }).listen(80, "", () =>
